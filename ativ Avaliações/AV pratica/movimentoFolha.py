5baf1fd866fdb53e5d4d6a67d19ec7d11e9bc911
Jonas Oliveira dos Santos <80828935+jonas410@users.noreply.github.com>
2021-05-21 13:30:16 -0300

    Update movimentoFolha.py

--- ativ Avaliações/AV pratica/movimentoFolha.py
+++ ativ Avaliações/AV pratica/movimentoFolha.py
@@ -1,9 +1,4 @@
 class MovimentoFolha():
-
-    #  Tipos de Movimento:          Exemplo:
-    #  'D' = Desconto               Plano de saúde: Provento (P)
-    #  'P' = Provento               Faltas: Desconto (D)
-
     def __init__(self, colaborador, descricao, valor, tipomovimento):
         self._colaborador = colaborador
         self._descricao = descricao
